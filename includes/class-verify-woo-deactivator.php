fc21a6ceefe2d36ad146cf52bfb610f19ac2f0fe
parsamirzaie <Mirzaie_parsa@protonmail.ch>
2025-07-20 18:47:04 +0330

    Initial commit for Verify Woo plugin, including core functionality, admin and public-facing components, styles, scripts, and internationalization support. Added README and necessary files for activation, deactivation, and uninstall processes.

---
+++ includes/class-verify-woo-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://parsamirzaie.com
+ * @since      1.0.0
+ *
+ * @package    Verify_Woo
+ * @subpackage Verify_Woo/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Verify_Woo
+ * @subpackage Verify_Woo/includes
+ * @author     Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
+ */
+class Verify_Woo_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ verify-woo.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://parsamirzaie.com
+ * @since             1.0.0
+ * @package           Verify_Woo
+ *
+ * @wordpress-plugin
+ * Plugin Name:       VerifyWoo
+ * Plugin URI:        https://#
+ * Description:       This is a description of the plugin.
+ * Version:           1.0.0
+ * Author:            Parsa Mirzaie
+ * Author URI:        https://parsamirzaie.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       verify-woo
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'VERIFY_WOO_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-verify-woo-activator.php
+ */
+function activate_verify_woo() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-verify-woo-activator.php';
+	Verify_Woo_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-verify-woo-deactivator.php
+ */
+function deactivate_verify_woo() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-verify-woo-deactivator.php';
+	Verify_Woo_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_verify_woo' );
+register_deactivation_hook( __FILE__, 'deactivate_verify_woo' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-verify-woo.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_verify_woo() {
+
+	$plugin = new Verify_Woo();
+	$plugin->run();
+
+}
+run_verify_woo();
