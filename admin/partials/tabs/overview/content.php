a701bf9b2f503367cd5b9853f0e8d99515946c24
parsamirzaie <Mirzaie_parsa@protonmail.ch>
2025-07-23 04:30:55 +0330

    Add method attribute to form in overview tab for proper data submission

--- admin/partials/tabs/overview/content.php
+++ admin/partials/tabs/overview/content.php
@@ -12,7 +12,7 @@
  */
 
 ?>
-<form class="modern-toggle-form" action="options.php">
+<form class="modern-toggle-form" action="options.php" method="POST">
 	<?php
 		settings_fields( 'verify_woo_settings_group' );
 		do_settings_sections( 'verify_woo_settings_page' );
